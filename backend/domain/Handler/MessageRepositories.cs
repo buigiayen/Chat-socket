1c7fb7f6090c6f0b98b40ed6581866ce663f305c
GIA IN <buiyen4696@gmail.com>
2025-06-25 17:01:26 +0700

    Refactor MessageRepositories and update interface
    - Added `InsertMessage` method to `MessageRepositories` for inserting messages into the database.
    - Removed `GetAllMessagesByUserAsync` and `GetMessageByIdAsync` methods from `MessageRepositories`.
    - Renamed `GetMessageUserID` to `MessageUser` without changing its implementation.
    - Updated `UpdateMessageStatusAsync` to include an implementation (currently throws `NotImplementedException`).
    - Removed `GetAllMessagesByUserAsync` and `GetMessageByIdAsync` from `IMessageRepositories` and added `InsertMessage`.

--- backend/domain/Handler/MessageRepositories.cs
+++ backend/domain/Handler/MessageRepositories.cs
@@ -8,21 +8,7 @@ namespace Server_chat.domain.Handler
 {
     public class MessageRepositories(IDbConnection dbConnection) : IMessageRepositories
     {
-        public Task<List<message>> GetAllMessagesByUserAsync(Guid userID)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<message?> GetMessageByIdAsync(Guid messageID)
-        {
-            throw new NotImplementedException();
-        }
-
-        public async Task<List<message>> GetMessageUserID(Guid userID, string centerID)
-        {
-            throw new NotImplementedException();
-        }
-
+        
         public async Task<bool> InsertMessage(message message)
         {
             string sql = "insert message (ToUser, FromUser, MessageText) values (@ToUser,@FromUser, @MessageText)";
@@ -32,13 +18,13 @@ namespace Server_chat.domain.Handler
         public async Task<IEnumerable<message>> MessageUser(Guid FromID, Guid ToID, DateTime FromTimeStamp, DateTime ToTimeStamp)
         {
             string sql = @"select top 50  m.messageID, m.MessageText, m.Timestamp , 1 as IsRead , m.FromUser from [dbo].[message] m
-                            where (m.FromUser=@FromUser and m.ToUser=@ToUser) or (m.FromUser=@ToUser and m.ToUser=@FromUser) and m.Timestamp between @FromTimeStamp and @ToTimeStamp
+                            where ((m.FromUser=@FromUser and m.ToUser=@ToUser) or (m.FromUser=@ToUser and m.ToUser=@FromUser)) and m.Timestamp between @FromTimeStamp and @ToTimeStamp
                             order by  m.Timestamp asc;";
 
             return await dbConnection.QueryAsync<message>(sql, new { FromUser = FromID, ToUser = ToID, FromTimeStamp = FromTimeStamp, ToTimeStamp = ToTimeStamp });
         }
 
-        public Task<bool> UpdateMessageStatusAsync(Guid messageID, bool isRead)
+        public async Task<bool> UpdateMessageStatusAsync(Guid messageID, bool isRead)
         {
             string sql = " update [dbo].[message] set IsRead=1 where (FromUser=@FromUser and ToUser=@ToUser) or (FromUser=@ToUser and ToUser=@FromUser) and Timestamp between @FromTimeStamp and @ToTimeStamp";
             throw new NotImplementedException();
--- backend/domain/repositories/IMessageRepositories.cs
+++ backend/domain/repositories/IMessageRepositories.cs
@@ -4,10 +4,7 @@ namespace Server_chat.domain.repositories
 {
     public interface IMessageRepositories
     {
-        Task<List<message>> GetAllMessagesByUserAsync(Guid userID);
-        Task<List<message>> GetMessageUserID(Guid userID, string centerID);
         Task<bool> UpdateMessageStatusAsync(Guid messageID, bool isRead);
-        Task<message?> GetMessageByIdAsync(Guid messageID);
         Task<bool> InsertMessage(message message);
         Task<IEnumerable<message>> MessageUser(Guid FromID, Guid ToID, DateTime FromTimeStamp, DateTime ToTimeStamp);
     }
